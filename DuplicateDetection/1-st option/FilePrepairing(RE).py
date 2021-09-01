7a1a0826dc8e7ab3920e11744a98cb53d6a2977e
Mostovik71 <56130198+Mostovik71@users.noreply.github.com>
2021-09-01 18:49:13 +0300

    Update FilePrepairing(RE).py

--- DuplicateDetection/1-st option/FilePrepairing(RE).py
+++ DuplicateDetection/1-st option/FilePrepairing(RE).py
@@ -11,15 +11,14 @@ df1=df[['row']]
 df1["row"]=df1["row"].apply(lambda x: x.lower())#Переводит весь верхний регистр в нижний
 df1["row"]=df1["row"].apply(lambda x: re.sub('\w*\d\w*','', x))#Убирает все цифры
 df1["row"]=df1["row"].apply(lambda x: re.sub('[%s]' % re.escape(string.punctuation), '', x))#Убирает пунктуацию (скобки, кавычки, и т.д.)
-df1["row"]=df1["row"].apply(lambda x: re.sub('http\w*','', x))#Убирает все слова, начинающиеся с http (ссылки)
-df1["row"]=df1["row"].apply(lambda x: re.sub('fujitsucom','', x))#Убирает все слова, начинающиеся с http (ссылки)
+
 def clean_text(text):
     text = re.sub(r"\n", " ", text)
     return text
 df1["row"] =df1["row"].apply(clean_text)
 
 df1=df1[df1['row'].str.contains('[а-я]')]
-#df1.to_csv('ideas2.csv',index=False,encoding='cp1251')
+
 
 
 train, test = train_test_split(df1, test_size=0.2,random_state=42)
